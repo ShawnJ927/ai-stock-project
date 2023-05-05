eaa5b43f019e00bf99bf574826638b18e7c29ae9
ShawnJ927 <shawn.jordan927@gmail.com>
2023-05-04 21:30:01 -0400

    Added Neural Net code sent from Sohail, fixed bugs

--- AIFinalProject.py
+++ AIFinalProject.py
@@ -1,16 +1,17 @@
 """
 This is an AI Project designed train an AI which will determine whether a stock price will 
 increase or decrease and the percentage change of the price by training the AI through 
-historical stock pricing data
+historical stock pricing data. 3 models are used, Logistic Regression, HMM, and Neural Network
 
-Authors: *** INSERT EVERYONE'S NAMES HERE *** Shawn Jordan
+Contributors: Shawn Jordan, Nathan Ngo, Sohail Quadir
 
-Note from Shawn - I've added initials to any comments I've made. Hopefully it gives 
+Note from Shawn to team - I've added initials to any comments I've made. Hopefully it gives 
 us somewhere to start.
 """
 
 import requests
 import numpy as np
+import tensorflow as tf
 # These imports were suggested by a ChatGPT search - SJ
 # To install them on pc, I used pip install scikit-learn
 from hmmlearn import hmm
@@ -67,7 +68,7 @@ def get_stock_historical_prices(data):
     
     return prices
 
-# This is a function that I might refactor and delete later since it is a bit redundant - SJ
+# This is a function that calls all the other calculations and returns their output - SJ
 def get_stock_direction_and_percentage_change(prices):
     """
     uses the stock prices to train the AI using two functions and
@@ -82,7 +83,7 @@ def get_stock_direction_and_percentage_change(prices):
     # algorithm n stuff go vroom here - SJ
     
     # once complete, return the direction and percentage change - SJ
-    return get_stock_direction_change(prices_last_5_years), get_stock_percentage_change(prices_last_5_years)
+    return get_stock_direction_change(prices_last_5_years), get_stock_percentage_change(prices_last_5_years), get_stock_percentage_change_neural_net(prices_last_5_years)
 
 # This is a function to learn from the given data to determine the direction of price change
 def get_stock_direction_change(prices_x_years):
@@ -108,7 +109,7 @@ def get_stock_direction_change(prices_x_years):
     # Make a prediction with LR Model for next day (modified from ChatGPT)
     y_prediction = logistic_regression.predict(x_test)
     # compare the actual data vs the prediction to determine the accuracy of the model
-    # I got this ide from ChatGPT
+    # I got this idea from ChatGPT
     accuracy = np.mean(y_prediction == y_test)
     print("Calculated Accuracy for LR Model:",accuracy)
     # return whether the price went up or down (binary)
@@ -147,32 +148,62 @@ def get_stock_percentage_change(prices_x_years):
 def get_stock_percentage_change_neural_net(prices_x_years):
     # need to preprocess the price data
     data_resizer = MinMaxScaler(feature_range=(0,1))
-    resized_data = data_resizer.fit_transform(prices_x_years)
+    resized_data = data_resizer.fit_transform(prices_x_years.reshape(-1,1))
     # need to split data into training set and testing set
+    train_data, test_data = train_test_split(resized_data, test_size=0.2, random_state=69)
 
     # create a dataset to loop through and return two np arrays 
+    def create_dataset(data, seq_length):
+        X = []
+        y = []
+        for i in range(len(data) - seq_length - 1):
+            X.append(data[i:(i+seq_length), :])
+            y.append(data[(i+seq_length), 0])
+        return np.array(X), np.array(y).reshape(-1,1)
     
+    steps = 10
     # fill training and test vars with new datasets  
-
-    # reshape the training and test data to be 3-dimentional
-
+    train_X, train_y = create_dataset(train_data, steps)
+    test_X, test_y = create_dataset(test_data, steps)
+    # reshape the training and test data to be 2-dimensional
+    #train_X = np.reshape(-1, (train_X.shape[0],train_X.shape[1], 1))
+    #test_X = np.reshape(-1, (test_X.shape[0], test_X.shape[1], 1))
     # create the neural network model
-
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.LSTM(1000, return_sequences=True, input_shape=(train_X.shape[1], train_X.shape[2])))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.LSTM(260))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Dense(1))
     # compile the model
-
+    model.compile(loss='mean_squared_error', optimizer='adam')
     # fit model to data
+    model.fit(train_X, train_y, epochs=50, batch_size=64, verbose=1)
 
     # make a prediction of the percentage change
+    predicted_percentage_change = model.predict(test_X)
+
+    # invert scaling
+    predicted_percentage_change = data_resizer.inverse_transform(predicted_percentage_change)[0][0]
+    test_y = data_resizer.inverse_transform(test_y)
+    
+    # check model accuracy
+    accuracy = np.mean((predicted_percentage_change - test_y)**2)
+    print("Neural Net Calculated Mean Squared Error", accuracy)
+    accuracy = np.mean(np.abs(predicted_percentage_change - test_y))
+    print("Neural Net Calculated Mean Absolute Error", accuracy)
+
+    return predicted_percentage_change
 
 
 # This will check the stock based on the symbol the user enters in the command line, 
 
 stock_symbol = input("Please enter a stock symbol you want to check\n").upper()
 historical_prices = get_stock_historical_prices(get_stock_data(stock_symbol))
-stock_direction, percentage_change = get_stock_direction_and_percentage_change(historical_prices)
+stock_direction, percentage_change, percentage_change_nn = get_stock_direction_and_percentage_change(historical_prices)
 if (stock_direction == 1):
-    print ("Predicted direction of change is increasing using Logistic Regression")
+    print ("\nPredicted direction of change is increasing using Logistic Regression")
 else:
-    print ("Predicted direction of change is decreasing using Logistic Regression")
+    print ("\nPredicted direction of change is decreasing using Logistic Regression")
 print("Using HMM: Predicted Percentage Change of",stock_symbol,"-", percentage_change, "%")
-
+print("Using Neural Net: Predicted Percentage Change of", percentage_change_nn, "%")
